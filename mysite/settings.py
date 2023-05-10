31a0ee3edd793a3a32df5f11bbe34e2068043f17
Emmanuel <123575837+SeaOptative@users.noreply.github.com>
2023-05-10 12:12:12 -0400

    Add files via upload

--- mysite/settings.py
+++ mysite/settings.py
@@ -128,3 +128,6 @@ ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_REDIRECT_URL = '/'
